43c8df9ca824b9497259d1d4f243ea60af2a43fd
DuuEyn <144405917+DuuEyn@users.noreply.github.com>
2024-03-18 01:19:52 +0800

    Create auth.py

---
+++ Packages/auth.py
@@ -0,0 +1,31 @@
+from urllib.parse import unquote
+
+from google_auth_oauthlib.flow import Flow
+
+
+def OAuthCall(SCOPES):
+  creds = None
+
+  #Ensure that the redirect_uri matches the one on client_secret.json
+  #If it does not, then the auth_url will return a redirect_uri mismatch error when accessed
+  flow = Flow.from_client_secrets_file(
+      'Packages/client_secret.json',
+      scopes=SCOPES,
+    redirect_uri='https://OMDSPythonRepl.iddiche.repl.co')
+  auth_url, _ = flow.authorization_url(prompt='consent')
+
+  print(f'Please go to this URL: {auth_url}')
+  code = input('\nEnter the authorization code: ')
+
+  #decode percent-encoded sequences into Unicode characters
+  flow.fetch_token(code=unquote(code)) 
+  
+  creds = flow.credentials
+  print('Auth success\n')
+
+  #Returns the generated credentials
+  return creds
+
+
+if __name__ == "__main__":
+  OAuthCall(SCOPES)
